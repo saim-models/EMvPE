303837d098f08c84e169a13bc0f3246db839dc11
Dietmar Heinke <d.g.heinke@bham.ac.uk>
2019-03-28 23:58:23 +0100

    Added comments

--- brownian_hopfield_euler.m
+++ brownian_hopfield_euler.m
@@ -5,6 +5,7 @@ function x = brownian_hopfield_euler(x_1, input, tau, sigma)
 %[n m] = size(input)
 % Brownian motion based on http://www.numericalexpert.com/blog/geometric_brownian_motion/
 % and delta t = 1;
+%
 [n m] = size(x_1);
 
 x = x_1 + 1/tau * (- x_1 -  input) + sigma * randn(n,m);
